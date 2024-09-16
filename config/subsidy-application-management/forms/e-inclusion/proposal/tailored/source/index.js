e156209fdd867838b357e048cfa4695663f65fb8
Achraf <achrafatauil@hotmail.com>
2024-09-16 14:56:58 +0200

    Cleanup

--- config/subsidy-application-management/forms/e-inclusion/proposal/tailored/source/index.js
+++
@@ -1,8 +0,0 @@
-const missingTypesExtractor = require('./extractors/missing-types-extractor');
-
-/**
- * NOTE: order of execution bound to position in the array
- */
-module.exports = [
-    missingTypesExtractor
-]
